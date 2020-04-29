4b1d39acd5d875d5aef168cf97dd59595ab941b4
santoshkumarradha <srr70@case.edu>
2020-04-29 11:58:37 -0400

    Initial commit

---
+++ _site/bin/send.sh
@@ -0,0 +1,5 @@
+python taggenerator.py
+git init
+git add .
+git commit -m "Initial commit"
+echo "y" | sh ./bin/deploy --user
---
+++ _tag/tight-binding.markdown
@@ -0,0 +1,6 @@
+---
+layout: tagpage
+title: "Tag: tight-binding"
+tag: tight-binding
+robots: noindex
+---
